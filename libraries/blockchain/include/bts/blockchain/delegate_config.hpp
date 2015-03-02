65fa78bf44abcfa0cf5a4ab953fd72b31b846440
oobinchu2@gmail.com <oobinchu2@gmail.com>
2015-03-02 13:20:14 +0000

    Updated configuration changes

--- libraries/blockchain/include/bts/blockchain/delegate_config.hpp
+++ libraries/blockchain/include/bts/blockchain/delegate_config.hpp
@@ -7,8 +7,12 @@
 #ifdef BTS_TEST_NETWORK
 #define NETWORK_MIN_CONNECTION_COUNT_DEFAULT 0
 #else
-#define NETWORK_MIN_CONNECTION_COUNT_DEFAULT 4
+#define NETWORK_MIN_CONNECTION_COUNT_DEFAULT 0
 #endif
+
+/**
+#define NETWORK_MIN_CONNECTION_COUNT_DEFAULT 0
+**/
 
 #define BTS_BLOCKCHAIN_AVERAGE_TRX_SIZE 512 // just a random assumption used to calibrate TRX per SEC
 /** defines the maximum block size allowed, 2 MB per hour */
--- libraries/client/include/bts/client/seed_nodes.hpp
+++ libraries/client/include/bts/client/seed_nodes.hpp
@@ -4,7 +4,10 @@ namespace bts { namespace client {
 
 static const std::vector<std::string> SEED_NODES
 {
-	"54.68.193.243:2810"
+	"54.68.193.243:2810",
+	"54.201.52.147:2810",
+	"54.186.40.201:2810"
+
 };
 
 } } // bts::client
